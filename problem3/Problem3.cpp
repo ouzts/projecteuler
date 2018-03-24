341ed9bb5dee0964638a33c31c9dd37863f6d222
Andrew Ouzts <andrew.ouzts@dynetics.com>
2018-03-24 11:37:09 -0500

    Problem 3 solution

---
+++ problem3/Problem3.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <cmath>
+
+bool isPrime(long long val)
+{
+  for (long long i = 2; i <= ceil(sqrt(val)); i++)
+  {
+    if (val % i == 0) return false;
+  }
+  return true;
+}
+
+int main()
+{
+  long long goal = 600851475143;
+  long long ans = -1;
+
+  for (long long i = ceil(sqrt(goal)); i >= 3; i--)
+  {
+    if (goal % i == 0 && isPrime(i))
+    {
+      ans = i;
+      break;
+    }
+  }
+
+  std::cout << ans << std::endl;
+  return 0;
+}
