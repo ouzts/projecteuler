5433e28679e53d1e26d46fb3743334c8256de22c
Andrew Ouzts <andrew.ouzts@dynetics.com>
2018-04-03 17:23:00 -0500

    Turns out this didn't work for all inputs, just the problem one!  Fixed.

--- problem3/Problem3.cpp
+++ problem3/Problem3.cpp
@@ -3,10 +3,8 @@
 
 bool isPrime(long long val)
 {
-  for (long long i = 2; i <= ceil(sqrt(val)); i++)
-  {
+  for (long long i = 2; i <= ceil(sqrt(val)) && i < val; i++)
     if (val % i == 0) return false;
-  }
   return true;
 }
 
@@ -15,13 +13,13 @@ int main()
   long long goal = 600851475143;
   long long ans = -1;
 
-  for (long long i = ceil(sqrt(goal)); i >= 3; i--)
+  for (long long i = 2; i <= ceil(sqrt(goal)); i++)
   {
+    long long otherFactor = goal / i;
     if (goal % i == 0 && isPrime(i))
-    {
-      ans = i;
-      break;
-    }
+      ans = std::max(ans, i);
+    else if (otherFactor*i == goal && isPrime(otherFactor))
+      ans = std::max(ans, otherFactor);
   }
 
   std::cout << ans << std::endl;
